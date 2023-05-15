b7ac68993ebb2d0430ff26c1c29c7b1ffbb45af9
kaminchu <kmchu.mail@gmail.com>
2023-05-15 13:34:07 +0900

    fmatter

--- src/damm.ts
+++ src/damm.ts
@@ -1,52 +1,62 @@
-import {get10x10, get26x26, get36x36, get62x62} from './tables'
+import { get10x10, get26x26, get36x36, get62x62 } from "./tables";
 
-function compute(table: number[][], input: number[]): number{
+function compute(table: number[][], input: number[]): number {
   return input.reduce((c, e) => table[c][e], 0);
 }
 
-export function computeNumber(input: string): string{
-  if(!/^\d+$/.test(input)){
-    throw new SyntaxError('only number')
+export function computeNumber(input: string): string {
+  if (!/^\d+$/.test(input)) {
+    throw new SyntaxError("only number");
   }
-  return '' + compute(get10x10(), input.split('').map(Number));
+  return "" + compute(get10x10(), input.split("").map(Number));
 }
 
-export function computeAlphabet(input: string){
-  if(!/^[A-Z]+$/.test(input)){
-    throw new SyntaxError('only alphabet')
+export function computeAlphabet(input: string) {
+  if (!/^[A-Z]+$/.test(input)) {
+    throw new SyntaxError("only alphabet");
   }
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  return alphabet.charAt(compute(get26x26(), input.split('').map((s) => alphabet.indexOf(s))));
+  return alphabet.charAt(
+    compute(get26x26(), input.split("").map((s) => alphabet.indexOf(s))),
+  );
 }
 
-export function computeAlphaNumeric(input: string){
-  if(!/^[A-Z0-9]+$/.test(input)){
-    throw new SyntaxError('only alphabet and number')
+export function computeAlphaNumeric(input: string) {
+  if (!/^[A-Z0-9]+$/.test(input)) {
+    throw new SyntaxError("only alphabet and number");
   }
-  const alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-  return alphaNumeric.charAt(compute(get36x36(), input.split('').map((s) => alphaNumeric.indexOf(s))));
+  const alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  return alphaNumeric.charAt(
+    compute(get36x36(), input.split("").map((s) => alphaNumeric.indexOf(s))),
+  );
 }
 
-export function computeMixedCaseAlphaNumeric(input: string){
-  if(!/^[A-Za-z0-9]+$/.test(input)){
-    throw new SyntaxError('only alphabet and number')
+export function computeMixedCaseAlphaNumeric(input: string) {
+  if (!/^[A-Za-z0-9]+$/.test(input)) {
+    throw new SyntaxError("only alphabet and number");
   }
-  const mixedCaseAlphaNumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  return mixedCaseAlphaNumeric.charAt(compute(get62x62(), input.split('').map((s) => mixedCaseAlphaNumeric.indexOf(s))));
+  const mixedCaseAlphaNumeric =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  return mixedCaseAlphaNumeric.charAt(
+    compute(
+      get62x62(),
+      input.split("").map((s) => mixedCaseAlphaNumeric.indexOf(s)),
+    ),
+  );
 }
 
-export function validateNumber(input: string): boolean{
-  return computeNumber(input) === '0';
+export function validateNumber(input: string): boolean {
+  return computeNumber(input) === "0";
 }
 
-export function validateAlphabet(input: string): boolean{
-  return computeAlphabet(input) === 'A';
+export function validateAlphabet(input: string): boolean {
+  return computeAlphabet(input) === "A";
 }
 
-export function validateAlphaNumeric(input: string): boolean{
-  return computeAlphaNumeric(input) === 'A';
+export function validateAlphaNumeric(input: string): boolean {
+  return computeAlphaNumeric(input) === "A";
 }
 
-export function validateMixedCaseAlphaNumeric(input: string): boolean{
-  return computeMixedCaseAlphaNumeric(input) === 'a';
+export function validateMixedCaseAlphaNumeric(input: string): boolean {
+  return computeMixedCaseAlphaNumeric(input) === "a";
 }
--- test/damm.test.ts
+++ test/damm.test.ts
@@ -1,44 +1,42 @@
 import {
-  computeAlphaNumeric,
   computeAlphabet,
+  computeAlphaNumeric,
   computeMixedCaseAlphaNumeric,
   computeNumber,
-  validateAlphaNumeric,
   validateAlphabet,
+  validateAlphaNumeric,
   validateMixedCaseAlphaNumeric,
-  validateNumber
+  validateNumber,
 } from "../src/damm";
-
 
 test("computeAlphaNumeric", () => {
   const input = "ABCDE123";
   const digit = computeAlphaNumeric(input);
-  expect(validateAlphaNumeric(input+digit)).toBe(true);
-  expect(validateAlphaNumeric(input+"A")).toBe(false);
-  expect(validateAlphaNumeric("ABCDE132"+digit)).toBe(false);
-})
+  expect(validateAlphaNumeric(input + digit)).toBe(true);
+  expect(validateAlphaNumeric(input + "A")).toBe(false);
+  expect(validateAlphaNumeric("ABCDE132" + digit)).toBe(false);
+});
 
 test("computeAlphabet", () => {
   const input = "ABCDEFG";
   const digit = computeAlphabet(input);
-  expect(validateAlphabet(input+digit)).toBe(true);
-  expect(validateAlphabet(input+"A")).toBe(false);
-  expect(validateAlphabet("ABCDEGF"+digit)).toBe(false);
-})
+  expect(validateAlphabet(input + digit)).toBe(true);
+  expect(validateAlphabet(input + "A")).toBe(false);
+  expect(validateAlphabet("ABCDEGF" + digit)).toBe(false);
+});
 
 test("computeMixedCaseAlphaNumeric", () => {
   const input = "ABCabc123";
   const digit = computeMixedCaseAlphaNumeric(input);
-  expect(validateMixedCaseAlphaNumeric(input+digit)).toBe(true);
-  expect(validateMixedCaseAlphaNumeric(input+"A")).toBe(false);
-  expect(validateMixedCaseAlphaNumeric("ABCabc132"+digit)).toBe(false);
-})
+  expect(validateMixedCaseAlphaNumeric(input + digit)).toBe(true);
+  expect(validateMixedCaseAlphaNumeric(input + "A")).toBe(false);
+  expect(validateMixedCaseAlphaNumeric("ABCabc132" + digit)).toBe(false);
+});
 
 test("computeNumber", () => {
   const input = "01234";
   const digit = computeNumber(input);
-  expect(validateNumber(input+digit)).toBe(true);
-  expect(validateNumber(input+"1")).toBe(false);
-  expect(validateNumber("12345"+digit)).toBe(false);
-})
-
+  expect(validateNumber(input + digit)).toBe(true);
+  expect(validateNumber(input + "1")).toBe(false);
+  expect(validateNumber("12345" + digit)).toBe(false);
+});
